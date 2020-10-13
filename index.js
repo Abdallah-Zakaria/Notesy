98e4679901dec52f4367a41218c8bd3632439c2f
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-10-13 17:08:28 +0300

    step1

--- index.js
+++ index.js
@@ -3,9 +3,23 @@
 
 const Input = require('./lib/input.js')
 const Handler = require('./lib/notes.js')
+const mongoose = require('mongoose');
+require('dotenv').config('.env');
+const MONGODB_URI = process.env.MONGODB_URI
+
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+
 
 
 const newInput = new Input()
 const newHandler = new Handler()
+
 newHandler.execute(newInput)
 
